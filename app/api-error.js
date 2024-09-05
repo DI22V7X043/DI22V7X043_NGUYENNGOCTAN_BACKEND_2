053aededf6d1401f83e3768c16aa27e14b082c71
DI22V7X043 <tandi22v7x043@dttx.ctu.edu.vn>
2024-09-05 21:21:20 +0700

    tiep backend 1

---
+++ app/api-error.js
@@ -0,0 +1,10 @@
+// app/api-error.js
+class ApiError extends Error {
+  constructor(statusCode, message) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
+
+module.exports = ApiError;
